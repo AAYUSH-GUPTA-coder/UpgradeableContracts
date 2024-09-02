eaa581d4b299c03cd771c972f4c5b0afb1be6b13
Aayush Gupta <guptaaayush432@gmail.com>
2024-09-02 14:54:23 +0530

    created BoxV1.sol

---
+++ src/BoxV1.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is UUPSUpgradeable, Initializable, OwnableUpgradeable {
+    uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); // Sets the owner of the contract to the sender
+        __UUPSUpgradeable_init(); // This function do nothing, but it is a good practice to have it to call this is UUPS upgradable contract and we are treating it as such
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    // function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
